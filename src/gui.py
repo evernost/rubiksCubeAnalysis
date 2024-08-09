a6ab41adc9f0c72957c123e86415e0ebb5d07ce7
Quentin Biache <nitrogenium@outlook.fr>
2024-08-09 20:53:41 +0200

    Establishing architecture

---
+++ src/gui.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# =============================================================================
+# Project         : Rubik's Cube Analysis
+# Module name     : GUI
+# File name       : gui.py
+# File type       : Python script (Python 3.10 or higher)
+# Purpose         : Graphical user interface for Rubik's Cube
+# Author          : QuBi (nitrogenium@outlook.fr)
+# Creation date   : August 8th, 2024
+# -----------------------------------------------------------------------------
+# Best viewed with space indentation (2 spaces)
+# =============================================================================
+
+# =============================================================================
+# Description
+# =============================================================================
+# Draws the Rubik's Cube using an abstract representation.
+# Inspired by a post from @jagarikin on X (November 19th, 2022).
+#
+
+
+
+
+# =============================================================================
+# External libs
+# =============================================================================
+import pygame
+
+
+
+
+
+
+
